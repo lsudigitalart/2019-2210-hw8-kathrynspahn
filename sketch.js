c8db9a56f2e054e6cc349b3034097af382ececb7
kathrynspahn <54597353+kathrynspahn@users.noreply.github.com>
2019-10-29 11:30:47 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,71 +1,37 @@
-var leg1, leg2, leg3;
-var head1, head2, head3, head4;
-
-var show = 0;
-var legChoice, headChoice;
 
 function preload(){
-    leg1 = loadImage("leg1.png");
-    leg2 = loadImage("leg2.png");
-    leg3 = loadImage("leg3.png");
+    leg1 = loadImage("images/leg1.png");
+    leg2 = loadImage("images/leg2.png");
+    leg3 = loadImage("images/leg3.png");
 
-    head1 = loadImage("head1.png");
-    head2 = loadImage("head2.png");
-    head3 = loadImage("head3.png");
-    head4 = loadImage("head4.png");
+    head1 = loadImage("images/head1.png");
+    head2 = loadImage("images/head2.png");
+    head3 = loadImage("images/head3.png");
+    head4 = loadImage("images/head4.png");
+}
+
+function setup() {
+  createCanvas(900, 1000)
+ imageMode(CENTER);
+
+}
+
+function mousePressed() {
+background(51, 40, 35)
+
+  leg(350, 550)
+  head(350, 100)  
+}
+
+function leg(x, y) {
+  let x = [leg1, leg2, leg3]
+  let legChoice = random(g)
+  image(legChoice, x, y, 420, 470)
 }
 
 
-function setup(){
-createCanvas(800,600);
-background(200,30,150);
+function body(x, y) {
+  let p = [head1, head2, head3]
+  let headChoice = random(y)
+  image(headChoice, x, y, 250, 320)
 }
-
-function draw() {
-    background(100);
-  
-    if (show == 1) {
-      creature(100, 50, legChoice, headChoice);
-    }
-  
-  }
-  
-  function mouseReleased() {
-    legChoice = int(random(4));
-    headChoice = int(random(4));
-    show = 1;
-  }
-  
-  function creature() {
-    
-
-  
-    if (legChoice == 0) {
-      image(leg1, (width / 2) - 60, (height / 2) + 20);
-    }
-  
-    if (legChoice == 1) {
-      image(leg2, (width / 2) - 60, (height / 2) + 20);
-    }
-  
-    if (legChoice == 2) {
-      image(leg3, (width / 2) - 60, (height / 2) + 20);
-    }
-  
-    if (headChoice == 0) {
-      image(head1, (width / 2) - 150, (height / 2) - 150);
-    }
-  
-    if (headChoice == 1) {
-      image(head2, (width / 2) - 150, (height / 2) - 150);
-    }
-  
-    if (headChoice == 2) {
-      image(head3, (width / 2) - 150, (height / 2) - 150);
-    }
-  
-    if (headChoice == 3) {
-      image(head4, (width / 2) - 150, (height / 2) - 150);
-    }
-  
-  }
